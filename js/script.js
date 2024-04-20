0c8e589cc12c2c65d7e91e7ee3c20108cdc615a2
shzdwg <universalpercept@gmail.com>
2024-04-20 10:03:44 +1000

    tut completed

--- js/script.js
+++ js/script.js
@@ -2,20 +2,22 @@ const choices = ["rock","scissors","paper"];
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
+    console.log(playerSelection,computerSelection);
+
     if (playerSelection == computerSelection) {
-        return "TIE";
+        return 1;
     } else if (playerSelection=="rock" && computerSelection=="scissors"){
-        return "PLAYERWINS";
+        return 0;
     } else if (playerSelection=="rock" && computerSelection=="paper"){
-        return "CPUWINS";
+        return 2;
     } else if (playerSelection=="paper" && computerSelection=="scissors"){
-        return "CPUWINS";
+        return 2;
     } else if (playerSelection=="paper" && computerSelection=="rock"){
-        return "PLAYERWINS";
+        return 0;
     } else if (playerSelection=="scissors" && computerSelection=="rock"){
-        return "CPUWINS";
+        return 2;
     } else if (playerSelection=="scissors" && computerSelection=="paper"){
-        return "PLAYERWINS";
+        return 0;
     }
   }
   
@@ -23,6 +25,24 @@ function playRound(playerSelection, computerSelection) {
     return randomElement = choices[Math.floor(Math.random() * choices.length)];
   }
 
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  function playGame(){
+    const playerSelection = "roCK";
+    let playerWins=0;
+    let computerWins=0;
+    let roundResult;
+
+    for (let i=0;i<5;i++){
+        let playerSelection = prompt("rock, scissors or paper?");
+        const computerSelection = getComputerChoice();
+        roundResult = playRound(playerSelection.toLowerCase(), computerSelection);
+        if (roundResult==0){
+            playerWins++;
+        } else if (roundResult==2){
+            computerWins++;
+        }
+    }
+
+    console.log("playerWins:" + playerWins + " computerWins:" + computerWins);
+  }
+  
+  playGame()
